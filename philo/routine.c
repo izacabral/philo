d9abaf3178e5e17cc5abab4152a36ce9f63f81d0
izacabral <izabelecabral@hotmail.com>
2023-04-03 22:21:52 +0000

    potencial deadlock ¨@%#¨&@%

--- philo/routine.c
+++ philo/routine.c
@@ -18,7 +18,7 @@ void	*routine(void *arg)
 
 	philo = arg;
 	if (philo->id % 2 == 0)
-		usleep(100);
+		usleep(10000);
 	while (check_is_died(philo))
 	{
 		//printf("id philo: %d\n", philo->id);
@@ -38,7 +38,9 @@ void	take_hashis(t_philo *philo)
 		pthread_mutex_lock(philo->right_hashi);
 		print_msg(philo, "has taken a fork");
 		print_msg(philo, "is eating");
+		//pthread_mutex_lock(&philo->m_time_last_meal);
 		philo->time_last_meal = get_time_now();
+		//pthread_mutex_unlock(&philo->m_time_last_meal);
 		pthread_mutex_lock(&philo->m_times_eated);
 		philo->times_eated++;
 		pthread_mutex_unlock(&philo->m_times_eated);
